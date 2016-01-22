9b1c20c49ed14382cf4d4dc1c283e04c45f591b8
Lokesh Mohan <lokeshm27@gmail.com>
2016-01-22 10:49:40 +0530

    Added Boolean Contin to Settings, Since Settings class Changed.

--- VLCMod/src/Settings.java
+++ VLCMod/src/Settings.java
@@ -5,10 +5,12 @@ public class Settings implements Serializable{
 	private static final long serialVersionUID = 1L;
 	boolean Start, Resume, Ask;
 	boolean High, Med, Low;
+	boolean Contin;
 	int resume, performance;
 	int secs;
 	
-	public Settings(int resume, int performance){
+	public Settings(int resume, int performance, boolean contin){
+		this.Contin = contin;
 		if((resume > 3 || resume < 1) || (performance > 3 || performance < 1)){
 			this.resume = 2;
 			this.performance = 2;
@@ -44,7 +46,7 @@ public class Settings implements Serializable{
 			Med = false;
 			High = true;
 		}
+		
 		secs = this.performance * 2000;
 	}
-
 }
